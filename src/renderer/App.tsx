80eb6b3f319981bccae6941a8ab56feea4f653db
JAVV <vinas.johnandrei@gmail.com>
2025-09-22 22:13:59 +0800

    installed tailwind plugin

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
+import './index.css';
+import SearchVehicle from './components/SearchVehicle';
 
 const App = () => {
   return (
     <div>
-      <h1>Hello from Electron and React! 👋🏼</h1>
+      <h1 className='font-bold underline text-red-700'>Hello from Electron and React! 👋🏼</h1>
       <p>This is a desktop application built with web technologies.</p>
+      <SearchVehicle />
     </div>
   )
 }
