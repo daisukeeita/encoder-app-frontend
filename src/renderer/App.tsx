3dce55d2c47266a996ac26f0d67ad6b8900d7b64
JAVV <vinas.johnandrei@gmail.com>
2025-09-24 21:37:23 +0800

    moved VehicleDetails component inside SearchVehicle component

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import SearchVehicle from './components/SearchVehicle';
-import VehicleDetails from './components/VehicleDetails';
 
 const App = () => {
   return (
     <>
       <h1 className='font-sans font-bold mt-3 ml-3 mb-5'>ENCODE VEHICLE</h1>
-      <div className='font-sans'>
-        <SearchVehicle />
-        <VehicleDetails />
+      <div className='mx-auto w-5/6'>
+        <div className='font-sans'>
+          <SearchVehicle />
+        </div>
       </div>
     </>
   );
