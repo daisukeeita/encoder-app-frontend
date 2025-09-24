267a0a5563797e8df011a7c96887bc2a0af99ad7
JAVV <vinas.johnandrei@gmail.com>
2025-09-24 21:35:30 +0800

    adding state for every input and moved VehicleDetail component inside this component

--- src/renderer/components/SearchVehicle.tsx
+++ src/renderer/components/SearchVehicle.tsx
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import fetchVehicleData from "../services/FetchVehicleData";
+import VehicleDetails from "./VehicleDetails";
 
+function formDataToObject(formData : FormData)  {
+  return Object.fromEntries(formData.entries())
+}
 
 export default function SearchVehicle (){
+  // TODO: CONVERT THIS INTO DROPDOWN STYLE
+
+  const [plateNumber, setPlateNumber] = useState('');
+  const [mvFileNumber, setMvFileNumber] = useState('');
+  const [chassis, setChassis] = useState('');
+  const [engine, setEngine] = useState('');
+  const [vin, setVin] = useState('');
+
+  const [inspectionID, setInspectionID] = useState('');
+  const [licensePlate, setLicensePlate] = useState('');
+  const [mvFileNo, setMvFileNo] = useState('');
+  const [chassisNo, setChassisNo] = useState('');
+  const [engineNo, setEngineNo] = useState('');
+  const [color, setColor] = useState('');
+  const [categoryType, setCategoryType] = useState('');
+  const [fuelType, setFuelType] = useState('');
+  const [modelYear, setModelYear] = useState('');
+
+
+  function handleSubmit (event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (event == null) {
+      console.error("event is null.");
+    } else {
+      const formData = formDataToObject(new FormData(event.currentTarget));
+
+      const vehicleData = fetchVehicleData(formData);
+
+      vehicleData.then(data => {
+        setInspectionID(data.inspectionId)
+        setLicensePlate(data.licensePlate)
+        setMvFileNo(data.mvFileNumber)
+        setChassisNo(data.chassis)
+        setEngineNo(data.engine)
+        setColor(data.color)
+        setCategoryType(data.categoryType)
+        setFuelType(data.fuelType)
+        setModelYear(data.modelYear)
+      }) 
+    }
+  } 
+
   return (
-      <form className="w-5/6 mx-auto mb-5 text-stone-700 font-sans">
+    <>
+      <form method="POST" onSubmit={ handleSubmit }>
         <fieldset className="fieldset bg-base-200 border border-base-300 rounded-box grid-cols-4 grid-rows-2 pb-5 pt-4 px-5">
           <legend className="fieldset-legend font-semibold ml-8">SEARCH VEHICLE</legend>
 
@@ -15,6 +64,8 @@ export default function SearchVehicle (){
               id="plate_no" 
               placeholder="Plate Number"
               className="input"
+              value={ plateNumber }
+              onChange={ e => setPlateNumber(e.target.value)}
             />
           </div>
 
@@ -26,6 +77,8 @@ export default function SearchVehicle (){
               id="mv_file_no"
               placeholder="MV File Number"
               className="input"
+              value={ mvFileNumber }
+              onChange={ e => setMvFileNumber(e.target.value)}
             />
           </div>
 
@@ -37,6 +90,8 @@ export default function SearchVehicle (){
               id="chassis_no" 
               placeholder="Chassis"
               className="input"
+              value={ chassis }
+              onChange={ e => setChassis(e.target.value)}
             />
           </div>
 
@@ -48,6 +103,8 @@ export default function SearchVehicle (){
               id="engine_no" 
               placeholder="Engine"
               className="input"
+              value={ engine }
+              onChange={ e => setEngine(e.target.value)}
             />
           </div>
 
@@ -55,11 +112,28 @@ export default function SearchVehicle (){
             type="text" 
             name="vin_no" 
             id="vin_no" 
-            style={{
-              display: 'none'
-            }}
+            style={{display: 'none'}}
+            value={ vin }
+            onChange={ e => setVin(e.target.value)}
           />
+
+          <div className="col-start-4 col-span-1 flex justify-center" style={{display: 'none'}}>
+            <button type="submit" className="btn btn-neutral btn-dash">Search</button>
+          </div>
         </fieldset>
       </form>
+
+      <VehicleDetails 
+        inspectionID={inspectionID}
+        licensePlate={licensePlate}
+        mvFileNo={mvFileNo}
+        chassisNo={chassisNo}
+        engineNo={engineNo}
+        color={color}
+        categoryType={categoryType}
+        fuelType={fuelType}
+        modelYear={modelYear} 
+      />
+    </>
   )
 }
