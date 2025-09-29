92bf9a7dd0a6c78ee764d188ece4b987bb1a6154
JAVV <vinas.johnandrei@gmail.com>
2025-09-29 21:53:47 +0800

    reformat contents

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import SearchVehicle from './components/SearchVehicle';
+import {VehicleDetailsContextProvider} from './contexts/VehicleDetailsContext';
+import VehicleDetails from './components/VehicleDetails';
 
 const App = () => {
   return (
     <>
-      <h1 className='font-sans font-bold mt-3 ml-3 mb-5'>ENCODE VEHICLE</h1>
-      <div className='mx-auto w-5/6'>
-        <div className='font-sans'>
-          <SearchVehicle />
+      <VehicleDetailsContextProvider>
+        <h1 className='font-sans font-bold mt-3 ml-3 mb-5'>ENCODE VEHICLE</h1>
+        <div className='mx-auto w-5/6'>
+          <div className='font-sans'>
+            <SearchVehicle />
+            <VehicleDetails />
+          </div>
         </div>
-      </div>
+      </VehicleDetailsContextProvider>
     </>
   );
 };
