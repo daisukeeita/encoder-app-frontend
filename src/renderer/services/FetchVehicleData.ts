4ed735afed5edbb85cfd62d1ffbfda99b70a65cf
JAVV <vinas.johnandrei@gmail.com>
2025-09-29 21:53:13 +0800

    removed catching the errors

--- src/renderer/services/FetchVehicleData.ts
+++ src/renderer/services/FetchVehicleData.ts
@@ -1,13 +1,19 @@
-export const basePath = "http://localhost:8080/api/v1/encoder/requestVehicle";
+const basePath = "http://localhost:8080/api/v1/encoder/requestVehicle";
 
 export default async function fetchVehicleData (requestBody: object) {
-  const response = await fetch(basePath, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(requestBody)
-  })
+    const response = await fetch(basePath, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(requestBody)
+    })
 
-  console.log(JSON.stringify(requestBody));
+    if (!response.ok) {
+      if (response.status === 404) throw new Error("Vehicle was not found.");
+      if (response.status === 500) throw new Error("There's an unexpected error in internal server.");
+      if (response.status === 503) throw new Error("The LTMS is currently unavailable or down.");
 
-  return await response.json();
+      throw new Error (`Unexpected error: ${response.status}`);
+    } 
+
+    return response.json();
 }
