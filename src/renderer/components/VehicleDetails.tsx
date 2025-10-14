5cd91d597dcafcf5d7bc3ccf069ea7223eb5537e
JAVV <vinas.johnandrei@gmail.com>
2025-10-14 09:54:28 +0800

    modified search vehicle component

--- src/renderer/components/VehicleDetails.tsx
+++ src/renderer/components/VehicleDetails.tsx
@@ -10,60 +10,93 @@ export default function VehicleDetails ()  {
     engineNo,
     color,
     categoryType,
+    manufacturer,
+    brand,
     fuelType,
-    modelYear
+    modelYear,
+    maximumTotalWeight
   } = useContext(VehicleDetailsContext);
 
   return (
-    <>
-      <ul className='list bg-base-100 rounded-box shadow-md mt-5'>
-        <li className='p-4 pb-2 text-xs opacity-60 tracking-wide'>VEHICLE DETAILS</li>
+    <div className='tab-content bg-base-100 border-base-300 p-6'>
+      <div className='grid grid-cols-2 gap-5'>
+        <ul className='list bg-base-100 rounded-box shadow-md'>
+          {/* INSPECTION ID */}
+          <li className='list-row'>
+            <div className='w-30'>Inspection ID: </div>
+            <div className='text-xs uppercase font-semibold opcity-60'>{inspectionID}</div>
+          </li>
 
-        <li className='list-row'>
-          <div className='w-30'>Inspection ID: </div>
-          <div className='text-xs uppercase font-semibold opcity-60'>{inspectionID}</div>
-        </li>
+          {/* LICENSE PLATE */}
+          <li className='list-row'>
+            <div className='w-30'>License Plate: </div>
+            <div className='text-xs uppercase font-semibold opcity-60'>{licensePlate}</div>
+          </li>
 
-        <li className='list-row'>
-          <div className='w-30'>License Plate: </div>
-          <div className='text-xs uppercase font-semibold opcity-60'>{licensePlate}</div>
-        </li>
+          {/* MV FILE NUMBER */}
+          <li className='list-row'>
+            <div className='w-30'>MV File Number: </div>
+            <div className='text-xs uppercase font-semibold opcity-60'>{mvFileNo}</div>
+          </li>
 
-        <li className='list-row'>
-          <div className='w-30'>MV File Number: </div>
-          <div className='text-xs uppercase font-semibold opcity-60'>{mvFileNo}</div>
-        </li>
+          {/* CHASSIS NUMBER */}
+          <li className='list-row'>
+            <div className='w-30'>Chassis: </div>
+            <div className='text-xs uppercase font-semibold opcity-60'>{chassisNo}</div>
+          </li>
 
-        <li className='list-row'>
-          <div className='w-30'>Chassis: </div>
-          <div className='text-xs uppercase font-semibold opcity-60'>{chassisNo}</div>
-        </li>
+          {/* ENGINE NUMBER */}
+          <li className='list-row'>
+            <div className='w-30'>Engine: </div>
+            <div className='text-xs uppercase font-semibold opcity-60'>{engineNo}</div>
+          </li>
 
-        <li className='list-row'>
-          <div className='w-30'>Engine: </div>
-          <div className='text-xs uppercase font-semibold opcity-60'>{engineNo}</div>
-        </li>
+          {/* VEHICLE COLOR */}
+          <li className='list-row'>
+            <div className='w-30'>Color: </div>
+            <div className='text-xs uppercase font-semibold opcity-60'>{color}</div>
+          </li>
+        </ul>
 
-        <li className='list-row'>
-          <div className='w-30'>Color: </div>
-          <div className='text-xs uppercase font-semibold opcity-60'>{color}</div>
-        </li>
+        
+        <ul className='list bg-base-100 rounded-box shadow-md'>
+          {/* CATEGORY TYPE */}
+          <li className='list-row'>
+            <div className='w-30'>Category Type: </div>
+            <div className='text-xs uppercase font-semibold opcity-60'>{categoryType}</div>
+          </li>
 
-        <li className='list-row'>
-          <div className='w-30'>Category Type: </div>
-          <div className='text-xs uppercase font-semibold opcity-60'>{categoryType}</div>
-        </li>
+          {/* MANUFACTURER */}
+          <li className='list-row'>
+            <div className='w-30'>Manufacturer: </div>
+            <div className='text-xs uppercase font-semibold opcity-60'>{manufacturer}</div>
+          </li>
 
-        <li className='list-row'>
-          <div className='w-30'>Fuel Type: </div>
-          <div className='text-xs uppercase font-semibold opcity-60'>{fuelType}</div>
-        </li>
+          {/* BRAND */}
+          <li className='list-row'>
+            <div className='w-30'>Brand: </div>
+            <div className='text-xs uppercase font-semibold opcity-60'>{brand}</div>
+          </li>
 
-        <li className='list-row'>
-          <div className='w-30'>Model Year: </div>
-          <div className='text-xs uppercase font-semibold opcity-60'>{modelYear}</div>
-        </li>
-      </ul>
-    </>
+          {/* FUEL TYPE */}
+          <li className='list-row'>
+            <div className='w-30'>Fuel Type: </div>
+            <div className='text-xs uppercase font-semibold opcity-60'>{fuelType}</div>
+          </li>
+
+          {/* MODEL YEAR */}
+          <li className='list-row'>
+            <div className='w-30'>Model Year: </div>
+            <div className='text-xs uppercase font-semibold opcity-60'>{modelYear}</div>
+          </li>
+
+          {/* MAXUMUM TOTAL WEIGHT */}
+          <li className='list-row'>
+            <div className='w-30'>Maximum Total Weight: </div>
+            <div className='text-xs uppercase font-semibold opcity-60'>{maximumTotalWeight}</div>
+          </li>
+        </ul>
+      </div>
+    </div>
   ) 
 }
