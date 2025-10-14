708d5bcab92a80b170f8e3785b82df541eb7ea1a
JAVV <vinas.johnandrei@gmail.com>
2025-10-14 09:55:57 +0800

    wrapping routes and contexts

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import SearchVehicle from './components/SearchVehicle';
-import {VehicleDetailsContextProvider} from './contexts/VehicleDetailsContext';
-import VehicleDetails from './components/VehicleDetails';
+import { SelectTabContextProvider } from './contexts/SelectTabContext';
+import { AuthenticationContextProvider } from './contexts/AuthenticationContext';
+import { HashRouter } from 'react-router';
+import { AppRoutes } from './components/Routes/AppRoutes';
+
 
 const App = () => {
+
   return (
     <>
-      <VehicleDetailsContextProvider>
-        <h1 className='font-sans font-bold mt-3 ml-3 mb-5'>ENCODE VEHICLE</h1>
-        <div className='mx-auto w-5/6'>
-          <div className='font-sans'>
-            <SearchVehicle />
-            <VehicleDetails />
-          </div>
-        </div>
-      </VehicleDetailsContextProvider>
+      <AuthenticationContextProvider>
+        <HashRouter>
+          <SelectTabContextProvider>
+            <AppRoutes /> 
+          </SelectTabContextProvider>
+        </HashRouter>
+      </AuthenticationContextProvider>
     </>
   );
 };
